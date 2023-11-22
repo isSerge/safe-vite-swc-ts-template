cb34260c7153d54430b3f242867b696e26f8f727
Serge Kovbasiuk <isserge@protonmail.com>
2023-11-22 15:46:15 +0700

    Remove loading text in AppLoader component

--- src/AppLoader.tsx
+++ src/AppLoader.tsx
@@ -2,7 +2,6 @@ export const AppLoader = () => (
   <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
     <div className="text-center">
       <p className="text-lg text-gray-700 mb-2">Waiting for Safe...</p>
-      <p className="text-sm text-gray-500">Loading...</p>
     </div>
   </div>
 );
